59618d3226ea58458fbc20277a34584501634675
Manasi Gore <mgore@nexient.com>
2020-07-04 13:39:52 -0400

    added vue router

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,87 +1,38 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
 import Header from "./components/layout/Header";
-import Todos from "./components/Todos";
-import AddTodo from "./components/AddTodo";
-
 export default {
   name: "App",
   components: {
     Header,
-    Todos,
-    AddTodo,
-  },
-  data() {
-    return {
-      todos: [],
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      fetch("https://jsonplaceholder.typicode.com/todos", {
-        method: "DELETE",
-      })
-        .then(() => (this.todos = this.todos.filter((todo) => todo.id !== id)))
-        .catch((err) => console.log(err));
-    },
-    addTodo(newTodo) {
-      const { title, completed } = newTodo;
-
-      fetch("https://jsonplaceholder.typicode.com/todos", {
-        method: "POST",
-        body: JSON.stringify({
-          title,
-          completed,
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => (this.todos = [...this.todos, json]))
-        .catch((err) => console.log(err));
-    },
-  },
-  created() {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
-      .then((response) => response.json())
-      .then((json) => {
-        this.todos = json;
-      })
-      .catch((err) => console.log(err));
   },
 };
 </script>
-
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
 }
 
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
+#nav {
+  padding: 30px;
 }
 
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
+#nav a {
+  font-weight: bold;
   color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
 }
 
-.btn:hover {
-  background: #666;
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ vue-app/src/views/Home.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from "../components/Todos";
+import AddTodo from "../components/AddTodo";
+
+export default {
+  name: "Home",
+  components: {
+    Todos,
+    AddTodo,
+  },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "DELETE",
+      })
+        .then(() => (this.todos = this.todos.filter((todo) => todo.id !== id)))
+        .catch((err) => console.log(err));
+    },
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      fetch("https://jsonplaceholder.typicode.com/todos", {
+        method: "POST",
+        body: JSON.stringify({
+          title,
+          completed,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => (this.todos = [...this.todos, json]))
+        .catch((err) => console.log(err));
+    },
+  },
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((json) => {
+        this.todos = json;
+      })
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
